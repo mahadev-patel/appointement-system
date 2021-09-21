f60418e4a2f25f004c55fc38dead6496742b5311
mahadev-patel <mahadevpatel660@gmail.com>
2021-09-21 20:34:10 +0530

    Update appointment.rb

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,5 +1,5 @@
 class Appointment < ApplicationRecord
   belongs_to :doctor, class_name: "User", foreign_key: "doctor_id"
   belongs_to :patient, class_name: "User", foreign_key: "patient_id"
-	has_many :appointment_records, dependent: :destroy
+  has_one :appointment_record, dependent: :destroy
 end
